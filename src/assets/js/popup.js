09381b3a1797982637c9b776ad72c283f2f2eeab
yoyal.limbu <yoyal.limbu@gmail.com>
2022-07-10 22:36:47 +0545

    added id to list and cleaned delete and update bug

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -1,24 +1,36 @@
 $(document).ready(function () {
-    function getNotes() {
-      chrome.storage.local.get(["notes"], function (result) {
-        let allNotes = result.notes.reverse();
+  function getNotes() {
+    chrome.storage.local.get(["notes"], function (result) {
+      if (result.notes) {
+        let allNotes = result.notes;
+        allNotes.sort(function (a, b) {
+          let aid = a.id;
+          let bid = b.id;
+          if (aid > bid) {
+            return -1;
+          } else if (aid < bid) {
+            return 1;
+          }
+          return 0;
+        });
         $(allNotes).each(function (key, index) {
+          var show = key == 0 && "show";
           var list =
             '<div class="col-12">\
           <div class="accordion" id="note-id-' +
-            key +
+            index.id +
             '">\
             <div class="accordion-item">\
               <div class="accordion-header" id="panelsStayOpen-heading-' +
-            key +
+            index.id +
             '">\
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse-' +
-            key +
+            index.id +
             '" aria-expanded="false" aria-controls="panelsStayOpen-collapse-' +
-            key +
+            index.id +
             '">\
               <h6 contenteditable style="width:80%; outline-width: 0 !important;"  class="nt-' +
-            key +
+            index.id +
             '">\
                    ' +
             index.title +
@@ -27,19 +39,21 @@ $(document).ready(function () {
                 </button>\
               </div>\
               <div id="panelsStayOpen-collapse-' +
-            key +
-            '" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-' +
-            key +
+            index.id +
+            '" class="accordion-collapse collapse ' +
+            show +
+            '" aria-labelledby="panelsStayOpen-heading-' +
+            index.id +
             '">\
                 <div class="accordion-body nb-' +
-            key +
+            index.id +
             '" contenteditable>\
                 ' +
             index.content +
             '  \
                 </div>\
                 <div class="d-flex m-2 gap-2" data-key = "' +
-            key +
+            index.id +
             '">\
                 <button class="btn btn-outline-primary btn-sm update-note"><i class="fa fa-save"></i></button>\
                 <button class="btn btn-outline-danger btn-sm delete-note" data-bs-toggle="modal" data-bs-target="#deleteNoteModal"><i class="fa fa-trash-can">Save</i></button>\
@@ -51,69 +65,78 @@ $(document).ready(function () {
           var listContainer = $("#note-list");
           $(listContainer).append(list);
         });
-      });
-    }
-    getNotes(); //call to load all notes
-    //utility function to call modal
-    function closeModal(modalName) {
-      $(`#${modalName}`).modal("hide");
-    }
-    //save new note
-    $("#save-note").on("click", () => {
-      const newNote = {
-        title: $(".modal-title").text(),
-        content: $("#note-area").text(),
-      };
-  
-      chrome.storage.local.get(["notes"], function (result) {
-        const allNotes = result.notes;
-        if (allNotes) {
-          allNotes.push(newNote);
-        }
-        chrome.storage.local.set({ notes: allNotes ?? [newNote] });
-        var listContainer = $("#note-list");
-        $(listContainer).empty();
-        getNotes();
-        $(".modal-title").empty().text("Title");
-        $("#note-area").empty().text("Start Typing");
-        closeModal("exampleModal");
-      });
-    });
-    //update note
-    $(document).on("click", ".update-note", function () {
-      var key = $(this).parent().data("key");
-      var title = $(`.nt-${key}`).html();
-      var content = $(`.nb-${key}`).html();
-      chrome.storage.local.get(["notes"], function (result) {
-        const allNotes = result.notes.reverse();
-        allNotes[key].content = content;
-        allNotes[key].title = title;
-        chrome.storage.local.set({ notes: allNotes.reverse() });
-      });
-    });
-    //delete note
-    $(document).on("click", ".delete-note", function () {
-      $("#deleteNoteModal").attr("data-delete-key", $(this).parent().data("key"));
-    });
-  
-    $("#confirm-delete").on("click", () => {
-      var key = $("#deleteNoteModal").attr("data-delete-key");
-      chrome.storage.local.get(["notes"], function (result) {
-        const allNotes = result.notes;
-        delete allNotes[key];
-        var filteredNotes = allNotes.filter(function (el) {
-          return el != null;
-        });
-        chrome.storage.local.set({ notes: filteredNotes });
-        $(`#note-id-${key}`).parent().remove();
-        closeModal("deleteNoteModal");
-      });
-    });
-    $(document).on("keypress", ".accordion-button", function (e) {
-      if (e.keyCode == 32) {
-        var val = $(this).children().find("h6").text();
-        $(this).children().find("h6").text(val+" ");
       }
     });
+  }
+  getNotes(); //call to load all notes
+  //utility function to call modal
+  function closeModal(modalName) {
+    $(`#${modalName}`).modal("hide");
+  }
+  //save new note
+  $("#save-note").on("click", () => {
+    const newNote = {
+      id: 0,
+      title: $(".modal-title").text(),
+      content: $("#note-area").text(),
+    };
+    chrome.storage.local.get(["notes"], function (result) {
+      const allNotes = result.notes;
+      if (allNotes.length > 0) {
+        let latestNote = allNotes.reverse()[0];
+        newNote.id = latestNote.id + 1;
+        console.log("new note", newNote);
+        allNotes.push(newNote);
+      }
+
+      chrome.storage.local.set({
+        notes: allNotes.length > 0 ? allNotes : [newNote],
+      });
+      var listContainer = $("#note-list");
+      $(listContainer).empty();
+      getNotes();
+      $(".modal-title").empty().text("Title");
+      $("#note-area").empty().text("Start Typing");
+      closeModal("exampleModal");
+    });
   });
-  
+  //update note
+  $(document).on("click", ".update-note", function () {
+    var key = $(this).parent().data("key");
+    var title = $(`.nt-${key}`).html();
+    var content = $(`.nb-${key}`).html();
+    chrome.storage.local.get(["notes"], function (result) {
+      const allNotes = result.notes;
+      var update = allNotes.find((e) => e.id == key);
+      update.content = content;
+      update.title = title;
+      chrome.storage.local.set({ notes: allNotes });
+    });
+  });
+  //delete note
+  $(document).on("click", ".delete-note", function () {
+    $("#deleteNoteModal").attr("data-delete-key", $(this).parent().data("key"));
+  });
+
+  $("#confirm-delete").on("click", () => {
+    var key = $("#deleteNoteModal").attr("data-delete-key");
+    chrome.storage.local.get(["notes"], function (result) {
+      const allNotes = result.notes;
+      var filteredNotes = allNotes.filter(function (el) {
+        return el.id != key;
+      });
+      chrome.storage.local.set({ notes: filteredNotes });
+      $(`#note-id-${key}`).parent().remove();
+      closeModal("deleteNoteModal");
+    });
+  });
+  $(document).on("keypress", ".accordion-button", function (e) {
+    if (e.keyCode == 32) {
+      var val = $(this).children().find("h6").text();
+      $(this)
+        .children()
+        .find("h6")
+        .text(val + " ");
+    }
+  });
+});
